57992697a07fdbd1ec6c806696b6dcc358b0cb62
unknown <pcadmin@DTPRWY07.proway.corp>
2021-02-09 19:47:24 -0300

    tipando arrays

--- fundamentos.ts
+++ fundamentos.ts
@@ -15,8 +15,6 @@ alert(meuHexa);
 let texto: String = "Ola Typescript";
 //alert (texto);
 
-
-
 let logradouro: string = "Rua 7 de Setembro";
 let numero: string = "123B";
 let cidade: string = "Blumenau";
@@ -25,3 +23,10 @@ alert("Endereço: " + logradouro + ", " + numero"." + cidade + " - " + estado);
 
 //com interpolação de string
 alert(`Endereço: ${logradouro}, ${numero}. ${cidade} - ${estado}`);
+
+// tipo array em TS
+let meuArray: number[] = [1, 2, 3, 4];
+alert(meuArray);
+
+//pode ser feito com o <> irá ter o mesmo efeito e com isso fica tipado o array
+let meuArray2: Array<number> = [1, 2, 3, 4];
